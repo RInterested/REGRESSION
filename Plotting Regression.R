c091984e98089306acb27fe5e61d3709f131da65
RInterested <RInterested@users.noreply.github.com>
2014-11-28 22:58:35 -0500

    Update Plotting Regression.R

--- Plotting Regression.R
+++ Plotting Regression.R
@@ -8,3 +8,11 @@ plot(Fertility ~ Agriculture)
 fit <-lm(Fertility ~ Agriculture)
 abline(fit)
 plot(fit)
+
+########OR
+
+data(swiss)
+head(swiss)
+fit<- lm(Fertility ~ Agriculture)
+plot(Fertility ~ Agriculture, pch=19,col="blue")
+lines(Agriculture, fit$fitted, lwd=3, col='green')
